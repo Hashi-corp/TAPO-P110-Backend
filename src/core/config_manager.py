984670812933360e62b60dfc97b281c3e768543c
Harish Krishnan P <138510321+Hashi-corp@users.noreply.github.com>
2025-07-01 16:07:51 +0530

    Delete src/core directory

--- src/core/config_manager.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from pathlib import Path
-
-class ConfigManager:
-    @staticmethod
-    def load_config(config_type: str):
-        with open(Path(__file__).parent.parent.parent / f"config/{config_type}_config.yaml") as f:
-            return yaml.safe_load(f)
